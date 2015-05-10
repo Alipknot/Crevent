15bac8720b14855dc8e9eaac746f335d701397cb
Nicolas <com16nico@gmail.com>
2015-05-10 13:34:41 -0400

    Agregada entidad para la tabla comuna, agregado combobox de comuna para busquedas

--- Buscar.php
+++ Buscar.php
@@ -4,8 +4,13 @@ include(__DIR__ .'/objects/class.categoria.php');
 include(__DIR__ .'/configuration.php');
 include(__DIR__ .'/objects/class.database.php');
 include(__DIR__ .'/objects/class.bempress.php');
+include(__DIR__ .'/objects/class.comuna.php');
 $Categoria = new Categoria();
 $catls= $Categoria->GetList();
+$comuna = new Comuna();
+
+$com=$comuna->GetList();
+
 
 
 
@@ -22,5 +27,15 @@ Buscar:<br>
                 echo("<option value='".$ct->DescCat."'>".$ct->DescCat."</option>");
      }?>
                 </select>
+                <select name="com">
+                        <?php
+
+
+                        foreach($com as $x){
+
+
+                          echo("<option value='".$x->NamComuna."'>".$x->NamComuna."</option>");
+               }?>
+                          </select>
 <input type="submit" value="Buscar">
 </form>
