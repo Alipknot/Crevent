a8e7484ff427fa079291ef8c67a4193903c718cb
Nicolas Alejandro Cancino Cancino <com16nico@gmail.com>
2015-05-11 18:42:31 -0300

    Actualizada class entidad comuna para hosting
    Se reemplazo el uso de mysqli_stmt::get_result por mysqli_stmt::bin_result para solucionar error de ejecución en servidor incompatible.
    Ver tambien Update Buscar.php.

--- objects/class.comuna.php
+++ objects/class.comuna.php
@@ -19,18 +19,17 @@ class Comuna
 			$pog_query = "select * from Comuna";
 			$stmt = $db->prepare($pog_query);
 			$stmt->execute();
+		
+				$stmt->bind_result($col1);
+  $devices = array();
 
-				$result= $stmt->get_result();
-				while($row = $result->fetch_object() )
-	{
-	$rows[]=$row;
-	}
-	return $rows;
-
-
-
-
-
+    while($stmt->fetch()) {
+        $tmp = array();
+        $tmp["NamComuna"] = $col1;
+        array_push($devices, $tmp);
+    }
+    $stmt->close();
+    return $devices;
 
 }
 }
